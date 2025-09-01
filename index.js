c44d46d3b389d95ee490755aa4e195db358ae7cb
David <davidkuron1996@gmail.com>
2025-09-02 00:01:58 +0300

    add this file

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 29;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 20);
